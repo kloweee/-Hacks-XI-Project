907df4b14df6fc6cfd5e4d82fecb0b524157d29c
lovey-lk <luciahe.md@gmail.com>
2025-09-27 18:38:20 -0400

    added comparisons

--- Extension/popup.js
+++ Extension/popup.js
@@ -1,11 +1,30 @@
 // popup.js
 
+const waterComparisons = [
+  { name: "hamburger", ml: 1750000, unit_text: "hamburgers"},
+  { name: "shower", ml: 65000, unit_text: "showers"},
+  { name: "water bottles", ml: 500, unit_text: "bottles of water"}
+];
+
+function getRandomWaterComparison(comparisons) {
+  const randomIndex = Math.floor(Math.random() * comparisons.length);
+  return comparisons[randomIndex];
+}
 // Function to update the popup UI
 function updatePopupUI(data) {
+  const totalWaterMl = parseFloat(data?.water_cost) || 0;
   document.getElementById("prompt").innerText = data?.prompt || "Not found";
   document.getElementById("response").innerText = data?.answer || "Not found";
   document.getElementById("count").innerText = data?.count || "0";
   document.getElementById("water_count").innerText = "You used "+ data?.water_cost + " mL(s) of water so far!" || "0.00";
+
+  const comparison = getRandomWaterComparison(waterComparisons);
+  let ratio = 0;
+  if (!isNaN(totalWaterMl) && comparison.ml > 0) {
+    ratio = (totalWaterMl / comparison.ml).toFixed(2);
+  }
+  document.getElementById("comparison").innerText = `That's equivalent to about ${ratio} ${comparison.unit_text}!`;
+
 }
 
 // 1. Initial data request when the popup opens
