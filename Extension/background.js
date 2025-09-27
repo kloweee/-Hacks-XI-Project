97e6f544e69f148c94bb6f969c2c97cc126fd9de
vjmurthy <vaidehijm25@gmail.com>
2025-09-27 14:21:36 -0400

    Update background.js

---
+++ Extension/background.js
@@ -0,0 +1,14 @@
+// background.js
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  if (changeInfo.status === 'complete') {
+    const isGemini = tab.url.includes('gemini.google.com') || 
+                     tab.url.includes('bard.google.com');
+    
+    if (!isGemini) {
+      // Disable extension functionality
+      chrome.action.disable(tabId);
+    } else {
+      chrome.action.enable(tabId);
+    }
+  }
+});
