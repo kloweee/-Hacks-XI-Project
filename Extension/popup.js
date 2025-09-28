b205fd19aa6419afa2a2124319107a79f371ae8c
lovey-lk <luciahe.md@gmail.com>
2025-09-28 09:37:42 -0400

    hidden count

--- Extension/popup.js
+++ Extension/popup.js
@@ -15,7 +15,7 @@ function updatePopupUI(data) {
   const totalWaterMl = parseFloat(data?.water_cost) || 0;
   document.getElementById("prompt").innerText = data?.prompt || "Not found";
   document.getElementById("response").innerText = data?.answer || "Not found";
-  document.getElementById("count").innerText = data?.count || "0";
+  //document.getElementById("count").innerText = data?.count || "0";
   document.getElementById("water_count").innerText = "You used "+ data?.water_cost + " mL(s) of water so far!" || "0.00";
 
   const comparison = getRandomWaterComparison(waterComparisons);
